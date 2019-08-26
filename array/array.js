9d6fd42a5ff65a03dd36da35bd38ab70b0d3b16d
Oghenebrume50 <raphlbrume@gmail.com>
2019-08-26 14:58:28 +0100

    done with testing

---
+++ array/array.js
@@ -0,0 +1,15 @@
+const arrayAnalysis = (array) => {
+  const avg = array.reduce((acc, value) => acc + value, 0) / array.length;
+  const min = array.reduce((prev, value) => value < prev ? value : prev);
+  const max = array.reduce((prev, value) => value > prev ? value : prev);
+  const length = array.length
+
+  return {
+    avg,
+    min,
+    max,
+    length
+  }
+}
+
+module.exports = arrayAnalysis;
--- caeser/caesar.test.js
+++ caeser/caesar.test.js
@@ -1,10 +1,14 @@
-var caesarMethods = require("./caesar.js");
+var caesarMethods = require("./caesar");
 
 test("testing Caesar's Cipher", () => {
-  expect(caesarMethods.caesar.cipher("string", 1)).toBe("tusjoh");
+  expect(caesarMethods.caesar.cipher("string", 0)).toBe("string");
+
+  expect(caesarMethods.caesar.cipher("string", 0)).not.toBe("STRING");
+
   expect(caesarMethods.caesar.cipher("abcdefghijklmnopqrstuvwxyz", 2)).toBe(
     "cdefghijklmnopqrstuvwxyzab"
   );
+  
   expect(
     caesarMethods.caesar.cipher("string% with&& 56! punc & numb009s", 4)
   ).toBe("wxvmrk% amxl&& 56! tyrg & ryqf009w");
